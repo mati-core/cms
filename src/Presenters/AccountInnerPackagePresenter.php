dc562b3d96d9582878a189abed87aba91fc58284
Martin Olmr <info@martinolmr.cz>
2021-02-16 15:07:41 +0100

    user account profile

---
+++ src/Presenters/AccountInnerPackagePresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\AdminModule\Presenters;
+
+
+use App\Presenters\BasePresenter;
+use Baraja\Doctrine\EntityManager;
+use MatiCore\User\UserPresenterAccessTrait;
+
+/**
+ * Class AccountInnerPackagePresenter
+ * @package App\AdminModule\Presenters
+ */
+class AccountInnerPackagePresenter extends BaseAdminPresenter
+{
+
+}
