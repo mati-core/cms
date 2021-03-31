206b5f84cf91890b0ffdd4e0da0dcc03bbd8ea64
Martin Olmr <info@martinolmr.cz>
2021-03-31 21:01:25 +0200

    fix setting presenter

--- src/Presenters/SettingInnerPackagePresenter.php
+++ src/Presenters/SettingInnerPackagePresenter.php
@@ -5,17 +5,7 @@ declare(strict_types=1);
 
 namespace App\AdminModule\Presenters;
 
-use Baraja\Doctrine\EntityManagerException;
-use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
-use Doctrine\ORM\NonUniqueResultException;
-use Doctrine\ORM\NoResultException;
 use MatiCore\Form\FormFactoryTrait;
-use MatiCore\User\Entity\User;
-use MatiCore\User\Entity\UserGroup;
-use MatiCore\User\UserManager;
-use Nette\Application\UI\Form;
-use Nette\Utils\ArrayHash;
-use Tracy\Debugger;
 
 /**
  * Class UserInnerPackagePresenter
